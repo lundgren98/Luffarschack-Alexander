301fde20b228a7f4252228d635acff036d333158
lundgren98 <a.k.o.lundgren@gmail.com>
2023-01-18 22:57:25 +0100

    Tests are now green.
    Changed pom.xml java source version to 1.8 for lambda support.

--- src/main/java/se/nackademin/Human.java
+++ src/main/java/se/nackademin/Human.java
@@ -1,8 +1,61 @@
 package se.nackademin;
 
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Human implements Player {
-	public int[] selectPlacement() { return new int[] {0,0}; }
-	private String getUserInput() { return ""; }
-	private boolean isInputValid(String str) { return false; }
-	private int[] stringToCordinates(String str) { return new int[] {0,0}; }
+	/**
+	 * Lets the user select a square on the board to place his piece on.
+	 * @return the cordinates for the piece.
+	 */
+	public int[] selectPlacement() {
+		String userInput;
+		do {
+			userInput = getUserInput();
+		} while (!isInputValid(userInput));
+		return stringToCordinates(userInput);
+	}
+
+	/**
+	 * Read input from user
+	 * @return the string the user wrote
+	 */
+	private String getUserInput() {
+		Scanner sc = new Scanner(System.in);
+		String userInput = sc.nextLine();
+		sc.close();
+		return userInput;
+	}
+
+	/**
+	 * Checks wether the string is of the form "x:y" where x and y
+	 * are integers
+	 * @param str the string which is checked.
+	 * @return a boolean indicating if such is the case.
+	 */
+	private boolean isInputValid(String str) {
+		String[] splt = str.split(":");
+		if (splt.length != 2)
+			return false;
+		try {
+			for (String s : splt) {
+				Integer.parseInt(s);
+			}
+		} catch (NumberFormatException e) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Converts strings of the form "x:y" to an integer array { x, y }
+	 * @param str the string to be converted.
+	 * @return an integer array of length 2 indicating the cordinates.
+	 */
+	private int[] stringToCordinates(String str) {
+		return Arrays.asList(str.split(":"))
+			.stream()
+			.map(s -> Integer.parseInt(s))
+			.mapToInt(i -> i).toArray();
+	}
 }
