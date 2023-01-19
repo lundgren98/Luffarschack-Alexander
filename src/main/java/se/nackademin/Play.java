f351273b84a9754d9452074da0a269130acf51ab
lundgren98 <a.k.o.lundgren@gmail.com>
2023-01-19 14:27:57 +0100

    Added comments and refactored Play.java

--- src/main/java/se/nackademin/Play.java
+++ src/main/java/se/nackademin/Play.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import se.nackademin.Board.PlacementState;
 import se.nackademin.Board.Square;
 
+/**
+ * A class to play a game of four in a row.
+ */
 public class Play extends GenericPlay {
 
 	public Play() {
@@ -14,21 +17,42 @@ public class Play extends GenericPlay {
 		this.playerList.add(new Human());
 	}
 
+	/**
+	 * Run the game
+	 */
 	public void run() {
+		while (playersTakeTurns());
+	}
+
+	/**
+	 * Let each player take a turn
+	 * @return a boolean indicating in the game should continue.
+	 */
+	private boolean playersTakeTurns() {
 		boolean firstPlayerTurn = true;
-		int[] pos = { -1, -1 };
-		while (true) {
-			for (Player player : this.playerList) {
-				System.out.println(this.board);
-				Square square = (firstPlayerTurn) ? Square.CIRCLE : Square.CROSS;
-				firstPlayerTurn = !firstPlayerTurn;
-				do {
-					pos = player.selectPlacement();
-				} while (this.board.tryToPlace(
-						pos[0],
-						pos[1],
-						square) != PlacementState.SUCCESS);
-			}
+		for (Player player : this.playerList) {
+			Square square = (firstPlayerTurn) ? Square.CIRCLE : Square.CROSS;
+			if (aPlayerTurn(player, square))
+				return false;
+			firstPlayerTurn = !firstPlayerTurn;
 		}
+		return true;
+	}
+
+	/**
+	 * Let one player play their turn.
+	 * @param player the player whose turn it is.
+	 * @param square the piece the player places.
+	 * @return a boolean indicating if the player has won.
+	 */
+	private boolean aPlayerTurn(Player player, Square square) {
+		int[] pos;
+		PlacementState placement;
+		System.out.println(this.board);
+		do {
+			pos = player.selectPlacement();
+			placement = this.board.tryToPlace(pos[0], pos[1], square);
+		} while (placement != PlacementState.SUCCESS);
+		return this.board.hasWon(square);
 	}
 }
