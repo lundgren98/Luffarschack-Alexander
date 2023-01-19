735c4941cc9004553b49b91e78e1c988cd738913
lundgren98 <a.k.o.lundgren@gmail.com>
2023-01-19 13:29:48 +0100

    Play now has two Human players taking turns placing pieces.

--- src/main/java/se/nackademin/Play.java
+++ src/main/java/se/nackademin/Play.java
@@ -1,8 +1,34 @@
 package se.nackademin;
 
+import java.util.ArrayList;
+
+import se.nackademin.Board.PlacementState;
+import se.nackademin.Board.Square;
+
 public class Play extends GenericPlay {
-	public void run() {
+
+	public Play() {
 		this.board = new Board(5);
-		System.out.println(this.board);
+		this.playerList = new ArrayList<Player>();
+		this.playerList.add(new Human());
+		this.playerList.add(new Human());
+	}
+
+	public void run() {
+		boolean firstPlayerTurn = true;
+		int[] pos = { -1, -1 };
+		while (true) {
+			for (Player player : this.playerList) {
+				System.out.println(this.board);
+				Square square = (firstPlayerTurn) ? Square.CIRCLE : Square.CROSS;
+				firstPlayerTurn = !firstPlayerTurn;
+				do {
+					pos = player.selectPlacement();
+				} while (this.board.tryToPlace(
+						pos[0],
+						pos[1],
+						square) != PlacementState.SUCCESS);
+			}
+		}
 	}
 }
