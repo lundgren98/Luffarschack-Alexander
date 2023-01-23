49131860c8809f697869ac0cd349a04e828ed9b2
lundgren98 <a.k.o.lundgren@gmail.com>
2023-01-23 11:21:31 +0100

    added ReacTurnsFromFile to FileIO

--- src/main/java/se/nackademin/FileIO.java
+++ src/main/java/se/nackademin/FileIO.java
@@ -1,20 +1,26 @@
 package se.nackademin;
 
 import se.nackademin.Play.PlayerWin;
+import se.nackademin.Board.Square;
 import java.util.List;
 import java.util.ArrayList;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.StandardOpenOption;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class FileIO {
+	private final String STATS_DIR = "stats/";
+	private final String STATS_FILE = STATS_DIR + "stat.csv";
+	private final String SAVES_DIR = "saves/";
+	private final String SAVES_FILE_PREFIX = SAVES_DIR + "save";
 	public void saveToFile(PlayerWin pw, int numberOfTurns) throws IOException {
-		Files.createDirectories(Paths.get("stats/"));
-		Path path = Paths.get("stats/stat.csv");
+		Files.createDirectories(Paths.get(STATS_DIR));
+		Path path = Paths.get(STATS_FILE);
 		String[] vals = new String[] {
 			pw.name(),
 			Integer.toString(numberOfTurns)
@@ -25,17 +31,17 @@ public class FileIO {
 		saveLinesToFile(lines, path);
 	}
 	public void saveToFile(List<Turn> turns) throws IOException {
-		Path dir = Paths.get("saves/");
+		Path dir = Paths.get(SAVES_DIR);
 		Files.createDirectories(dir);
 		int fileNumber = dir.toFile().list().length;
 		Path path = Paths.get(
-				"saves/save"
+				SAVES_FILE_PREFIX
 				+ Integer.toString(fileNumber)
 				+ ".csv");
 		List<String> lines = turnListToStringList(turns);
 		saveLinesToFile(lines, path);
 	}
-	public void saveLinesToFile(List<String> lines, Path path) throws IOException {
+	private void saveLinesToFile(List<String> lines, Path path) throws IOException {
 		for (String line : lines) {
 			StandardOpenOption opt = (path.toFile().exists())
 				? StandardOpenOption.APPEND
@@ -59,5 +65,23 @@ public class FileIO {
 	private String toCSV(String[] vals) {
 		return String.join(",", vals) + "\n";
 	}
-	public void ReadFromFile() {}
+	public List<Turn> ReadTurnsFromFile() throws IOException, IndexOutOfBoundsException {
+		Path file = Paths.get(SAVES_FILE_PREFIX + ".csv");
+		return Files.readAllLines(file)
+			.stream()
+			.map(s -> s.split(","))
+			.map(arr -> strArrToTurn(arr))
+			.toList();
+	}
+	private Turn strArrToTurn(String[] arr) throws IndexOutOfBoundsException {
+		String pieceStr = arr[0];
+		int row = Integer.parseInt(arr[2]);
+		int col = Integer.parseInt(arr[1]);
+		Square piece = switch (pieceStr) {
+			case "X" -> Square.CROSS;
+			case "O" -> Square.CIRCLE;
+			default -> Square.AVAILABLE;
+		};
+		return new Turn(piece, row, col);
+	};
 }
